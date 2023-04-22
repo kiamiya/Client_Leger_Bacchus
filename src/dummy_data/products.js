ab37b40fc1c90ae30c97fc88f77266a9a5d19adb
flashjaysan <jaysan@wanadoo.fr>
2023-04-22 16:37:27 +0200

    replace prices from string to number

--- src/dummy_data/products.js
+++ src/dummy_data/products.js
@@ -10,7 +10,7 @@ export const products = [
     "descriptionLong": "La cuvée l'Éclat offre une robe pâle aux reflets verts. Le nez frais et fruité présente des notes d'agrumes (pamplemousse, citron) et de fruits exotiques (litchi). En bouche, le vin est frais, rond et se termine sur des notes délicieusement fruitées. L'Eclat du domaine de Joÿ est un vin de plaisir à boire jeune, entre amis, à l'apéritif, sur des fruits de mer et crustacés, des poissons grillés, du Comté, des fromages de chèvres frais !",
     "imageFileName": "l-eclat-2021-domaine-de-joy.png",
     "imageAlt": "Bouteille de vin L'éclat 2021 - Domaine de Joÿ",
-    "price": "7€60"
+    "price": 7.60
   },
   {
     "id": "5264",
@@ -23,7 +23,7 @@ export const products = [
     "descriptionLong": "Chaque gorgée dégustée est une véritable explosion de fruits (d'agrumes), accompagnée d'une agréable fraîcheur qui fera appel à une nouvelle gorgée. Le conseil de nos sommeliers : avoir toujours une bouteille au frais... au cas où !",
     "imageFileName": "classic-2021-domaine-tariquet.png",
     "imageAlt": "Bouteille de vin Tariquet Classic 2021",
-    "price": "7€65"
+    "price": 7.65
   },
   {
     "id": "8745",
@@ -36,7 +36,7 @@ export const products = [
     "descriptionLong": "Dans une robe limpide et brillante, le vin offre un nez plein de senteurs sur des notes de fleurs, de fruits mûrs et de fruits confits. L'attaque est franche pour une bouche souple et suave, très équilibrée et persistante. Ce millésime révèle une intensité et une concentration sans égale!",
     "imageFileName": "harmonie-de-gascogne-blanc-2022-domaine-pellehaut.png",
     "imageAlt": "Bouteille de vin Harmonie de Gascogne Blanc 2022",
-    "price": "9€20"
+    "price": 9.20
   },
   {
     "id": "1564",
@@ -49,7 +49,7 @@ export const products = [
     "descriptionLong": "Des amis qui débarquent à l’improviste ? Une envie de s’ouvrir un bouteille de rouge, comme ça, juste pour le plaisir d’accompagner un morceau de fromage et un peu de charcuterie ? Avec Montagne Noire du Château d’Auzias on a tout bon ! Ses notes de fruits noirs et d’épices délicates agrémentent à merveille ce petit vin rouge du Languedoc au rapport qualité-prix-plaisir imbattable ! Foncez !",
     "imageFileName": "montagne-noire-2021-chateau-auzias.png",
     "imageAlt": "Bouteille de vin Montagne Noire 2021 - Chateau Auzias",
-    "price": "6€90"
+    "price": 6.90
   },
   {
     "id": "8749",
@@ -62,7 +62,7 @@ export const products = [
     "descriptionLong": "L'intégralité du vignoble d'Uby est certifié Viticulture Raisonnée Contrôlée depuis 10 ans et les vignes issues de l'agriculture biologique représentent 70 hectares du domaine. Cette cuvée rosée de la gamme Byo est un vrai régal... Avec des notes de framboise, de groseilles, le vin dégage une grande fraîcheur avec une bouche grasse et ample sur une délicieuse finale de fruits frais... Une pure gourmandise !",
     "imageFileName": "uby-byo-rose-n26-2021-domaine-uby.png",
     "imageAlt": "Bouteille de vin Uby Byo Rosé N°26 2021 - Domaine Uby",
-    "price": "7€90"
+    "price": 7.90
   },
   {
     "id": "9546",
@@ -75,7 +75,7 @@ export const products = [
     "descriptionLong": "Le Château Tour Bayard est une propriété familiale qui s'étend sur 15 hectares. Les vins sont réputés pour leur caractère affirmé qui exprime avec élégance la puissance du terroir. Julien Richard (propriétaire) recherche toujours l'équilibre entre la maturité du fruit et la fraîcheur à travers des vins denses, souples, des tanins fins et soyeux et une belle acidité. Ce millésime 2019 est une nouvelle fois une grande réussite. Joliment équilibré et déjà délicieux, c'est un vin d'une distinction notable. Découvrez toute la grandeur de ce vin exceptionnel à prix canon!",
     "imageFileName": "chateau-tour-bayard-2019.png",
     "imageAlt": "Bouteille de vin Chateau Tour Bayard 2019",
-    "price": "17€70"
+    "price": 17.70
   },
   {
     "id": "123",
@@ -88,7 +88,7 @@ export const products = [
     "descriptionLong": "Ce Château Poitevin 2016 affiche une belle robe couleur rubis. Il offre un nez intense sur des arômes complexes de tabac, de fruits noirs. En bouche, on retrouve un vin rond et soyeux avec des tanins veloutés, et une acidité bien maitrisée. Avec une belle finesse, un bel équilibre et une longue finale, ce Médoc a tout pour lui! ",
     "imageFileName": "chateau-poitevin-2016-cru-bourgeois.png",
     "imageAlt": "Bouteille de vin Chateau Poitevin 2016 Cru bourgeois",
-    "price": "17€50"
+    "price": 17.50
   },
   {
     "id": "22",
@@ -101,7 +101,7 @@ export const products = [
     "descriptionLong": "Le millésime 2018 du Château Reverdi présente une belle robe grenat aux reflets violines. Le nez est noble et dévoile des notes de café torréfié, de grillé. La bouche est fluide, sur la finesse avec des tanins très fondus. La longue finale sur les épices douces vient conclure une délicieuse dégustation. Ce Listrac-Médoc est une très belle découverte à déguster sans hésiter!",
     "imageFileName": "chateau-reverdi-2018-cru-bourgeois.png",
     "imageAlt": "Bouteille de vin Chateau Reverdi 2018 Cru bourgeois",
-    "price": "16€50"
+    "price": 16.50
   },
   {
     "id": "11",
@@ -114,7 +114,7 @@ export const products = [
     "descriptionLong": "Commercialisée à partir du millésime 2002, La Chapelle de Potensac est un second vin élaboré dans le respect des traditions, jouant d’un équilibre subtil basé sur la rondeur et la fraîcheur. Bénéficiant d’une saison particulièrement chaude, le millésime 2014 offre un vin racé et épicé, avec des notes complexes de fruits rouges et noirs. Un Bordeaux soyeux, mur et virile : une valeur sure à prix plancher !",
     "imageFileName": "la-chapelle-de-potensac-2014-second-vin-du-chateau-potensac.png",
     "imageAlt": "Bouteille de vin La Chapelle de Potensac 2014 Second vin du Chateau Potensac",
-    "price": "13€90"
+    "price": 13.90
   },
   {
     "id": "56",
@@ -127,7 +127,7 @@ export const products = [
     "descriptionLong": "Le Château Haut Musset a été repris par la famille Rambeaud en 2014. Ce Domaine de 6,8 hectares labellisé Haute Valeur Environnementale possède des vignes âgées de 35 ans en moyenne et travaillées en viticulture raisonnée. Ce Montagne Saint-Emilion est élevé 30% en cuve et 70% en barriques de chêne Français pendant 12 mois. Le résultat nous offre un vin puissant et expressif, où l’élégance du bois souligne la fraîcheur du fruit et affine l’équilibre charnu de la bouche. A apprécier dès à présent, il pourra également se garder en cave 5 à 15 ans.",
     "imageFileName": "chateau-haut-musset-2018.png",
     "imageAlt": "Bouteille de vin Chateau Haut Musset 2018",
-    "price": "18€90"
+    "price": 18.90
   },
   {
     "id": "826",
@@ -140,7 +140,7 @@ export const products = [
     "descriptionLong": "Un joli Viognier d'une couleur vive au nez riche et élégant ! Très belle aromatique avec ses arômes de fruits blancs et ses notes florales. En bouche la fraicheur vient se mêlée à un moelleux, une richesse concentrée et des pointes grillées en finale ! C'est persistant et très bien réalisé !",
     "imageFileName": "viognier-pays-d-oc-2022-vignobles-paul-mas.png",
     "imageAlt": "Bouteille de vin Viognier Pays d'Oc 2022 Vignobles Paul Mas",
-    "price": "6€75"
+    "price": 6.75
   },
   {
     "id": "642",
@@ -153,7 +153,7 @@ export const products = [
     "descriptionLong": "Avec son prix mini, sa réputation qui n’est plus à faire et son étiquette sympathique, le Chardonnay des Jamelles a tout ce qu’il faut pour vous séduire vous et vos convives. Que ça soit à l’apéritif ou autour d’un repas, son style bourguignon, légèrement vanillé et sa profusion de notes fruitées mettra tout le monde d’accord. Voilà ce qu’on appelle une valeur sûre !",
     "imageFileName": "chardonnay-2021-les-jamelles.png",
     "imageAlt": "Bouteille de vin Chardonnay 2021 Les Jamelles",
-    "price": "8€90"
+    "price": 8.90
   },
   {
     "id": "721",
@@ -166,7 +166,7 @@ export const products = [
     "descriptionLong": "Quelle belle surprise que cette cuvée \"Estive Blanc\" qui est ressortie grande première lors notre dégustation à l'aveugle. Plongez dans ses arômes d’ananas, de citron et de poire, et appréciez la fraicheur et la minéralité qu'il offre en bouche. Un blanc parfait pour un apéritif ou sur un poisson !",
     "imageFileName": "estive-blanc-2021-mas-des-cabres.png",
     "imageAlt": "Bouteille de vin Estive blanc 2021 Mas des Cabres",
-    "price": "8€95"
+    "price": 8.95
   },
   {
     "id": "444",
@@ -179,7 +179,7 @@ export const products = [
     "descriptionLong": "Le Picpoul de la Cave de Florensac est un vin d'une belle vivacité, avec des arômes d'agrumes, de fruit à chair blanche. La bouche est fraîche sur des notes de citron, de pamplemousse. Il est difficile de s'en passer lorsque l'on se régale de fruits de mer et autres poissons, sa qualité lui permet de sublimer les plus belles pièces ! Idéal également à l'apéritif, c'est un vin régulier qui peut se boire en toute saison sans jamais vous décevoir.",
     "imageFileName": "picpoul-2022-vignerons-de-florensac.png",
     "imageAlt": "Bouteille de vin Picpoul 2022 Vignerons de Florensac",
-    "price": "8€90"
+    "price": 8.90
   },
   {
     "id": "696",
@@ -192,7 +192,7 @@ export const products = [
     "descriptionLong": "Ce Picpoul de Pinet riche en fraîcheur viendra enchanter vos papilles grâce à ses arômes floraux et légèrement citronnés. Née de son terroir méditerranéen près du Bassin de Thau, le climat est tout simplement parfait pour créer ce vin d'exception: soleil et salinité. Une minéralité en bouche qui conviendra tout à fait pour accompagner crustacés et viande blanche.",
     "imageFileName": "picpoul-de-pinet-2021-domaine-morin-langaran.png",
     "imageAlt": "Bouteille de vin Picpoul de Pinet 2021 Domaine Morin-Langaran",
-    "price": "8€60"
+    "price": 8.60
   },
   {
     "id": "602",
@@ -205,7 +205,7 @@ export const products = [
     "descriptionLong": "Après 7 années de recherches et de travail, Jean-Claude Mas peut enfin présenter sa nouvelle gamme qui s'étend des vins de cru aux vins biologiques en passant par des vins élevés en biodynamie. Parmi eux, Jean-Claude Mas nous offre la Cuvée Secrète Chardonnay Sans Sulfite. Dotée d'un nez intense, c'est une cuvée complexe qui possède des arômes savoureux de brioche, de noisette et de coing. Elle est dans l'ensemble très fruitée, fait preuve d'une grande fraîcheur et se termine sur une belle finale, longue et équilibrée !",
     "imageFileName": "cuvee-secrete-2021-chardonnay-sans-sulfite-jean-claude-mas-domaines-paul-mas.png",
     "imageAlt": "Bouteille de vin Cuvée secrète 2021 Chardonnay Sans sulfite Jean-claude Mas Domaines Paul Mas",
-    "price": "9€95"
+    "price": 9.95
   },
   {
     "id": "800",
@@ -218,7 +218,7 @@ export const products = [
     "descriptionLong": "Ce rosé nous laisse sous le charme... Dans sa belle robe lumineuse aux reflets saumonés, le vin dégage un nez agréable et délicat sur des belles notes aromatiques de fruits rouges, d'agrumes et de pêche. Tout en fraîcheur et gourmandise, la bouche offre des arômes d'agrumes et petits fruits rouges avec une belle persistance très agréable.. N'hésitez plus, c'est le rosé parfait pour vos apéritifs et repas festifs !",
     "imageFileName": "corail-2021-chateau-de-roquefort.png",
     "imageAlt": "Bouteille de vin Corail 2021 Chateau de Roquefort",
-    "price": "14€00"
+    "price": 14.00
   },
   {
     "id": "1204",
@@ -231,7 +231,7 @@ export const products = [
     "descriptionLong": "Dominé par la montagne Sainte-Victoire, célèbre emblème de la région, le Domaine des Masques se déploie à 500m d'altitude dans un cadre exceptionnel. Cette splendide cuvée Essentielle Rosé se dévoile dans une robe rose franc, brillante et limpide. Le nez offre des notes d'agrumes (pamplemousse, citron vert) et fruits à noyaux (cerise, pêche) avec des nuances de menthe et de réglisse. Ample et fraîche, la bouche est dotée d'un bel équilibre, à la fois sur la tension et la gourmandise.. Un excellent rapport prix-plaisir !",
     "imageFileName": "essentielle-rose-2022-domaine-des-masques.png",
     "imageAlt": "Bouteille de vin Essentielle Rosé 2022 Domaine des Masques",
-    "price": "14€40"
+    "price": 14.40
   },
   {
     "id": "2001",
@@ -244,7 +244,7 @@ export const products = [
     "descriptionLong": "Les Maîtres Vignerons de la presqu'île de Saint-Tropez sont partenaires officiels de l'évènement des Voiles de Saint-Tropez. Des jus rigoureusement sélectionnés, conditionnés dans un flacon chic et décontracté. Il en résulte un vin à la robe rose très pâle. Le nez est intense sur des notes de pamplemousse, litchi, mangue. En bouche, le vin est structuré, ample, avec une belle expression et de la matière. La longue finale rafraîchissante vient clôturer une délicieuse dégustation.",
     "imageFileName": "les-voiles-2021-maitres-vignerons-de-la-presqu-ile-de-saint-tropez.png",
     "imageAlt": "Bouteille de vin Les Voiles 2021 Maitres Vignerons de la presqu'ile de Saint-Tropez",
-    "price": "14€00"
+    "price": 14.00
   },
   {
     "id": "5005",
@@ -257,6 +257,6 @@ export const products = [
     "descriptionLong": "Là encore, convivialité et générosité restent des piliers de la philosophie de vie de la famille Chapoutier. Ce Côtes de Provence frais, fruité et onctueux est parfait à partager entre amis. C'est tout simplement LA gourmandise idéale pour accompagner vos apéritifs, grandes tablées ou encore barbecue. Mathilde Chapoutier a su profiter de l’expérience de son père Michel, considéré comme l’un des vignerons les plus doués de sa génération.",
     "imageFileName": "grand-ferrage-2022-cuvee-orsuro-mathilde-chapoutier-selection.png",
     "imageAlt": "Bouteille de vin Grand Ferrage 2022 Cuvée Orsuro Mathilde Chapoutier Sélection",
-    "price": "12€90"
+    "price": 12.90
   },
 ];
